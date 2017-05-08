b4b788f13d34666df98cccc6ebf6b41aa225b515
KristenZhao <zhao.jianting912@gmail.com>
2017-05-08 12:58:15 -0400

    fixed ui layout

--- ui.R
+++ ui.R
@@ -39,35 +39,48 @@ dashboardPage(
                 column(width= 12,
                        box(width = NULL))
               )
+      ),
+      tabItem(tabName = "graphs",
+              fluidRow(
+                column(width = 6,
+                       box(width = NULL,
+                           plotlyOutput("capitalGraph"))),
+                column(width = 6,
+                       box(width = NULL,
+                           plotlyOutput("monroeGraph")
+                       )
+                )
+              ),
+              fluidRow(
+                column(width = 4,
+                       box(width = NULL,
+                           checkboxGroupInput('hourOfDay', 'Select the hours of day you are interested', 
+                                              choices = c(0:23), selected = NULL, inline = TRUE)
+                           )
+                       ),
+                column(width = 4,
+                       box(width = NULL,
+                           checkboxGroupInput('dayOfWeek', 'Select the days of week you are interested',
+                                              choices = c('Mon','Tue','Wed','Thu','Fri','Sat','Sun'),
+                                              selected = NULL, inline = FALSE)
+                           )
+                       ),
+                column(width = 4,
+                        box(width = NULL,
+                            checkboxGroupInput('monthOfYear', 'Select the months of year you are interested',
+                                               choices = c(1:12), inline = TRUE
+                                               )
+                            )
+                       )
+                   )
+              ),
+      tabItem(tabName = "about",
+              fluidRow(
+                column(width = 12,
+                       box(width = NULL,
+                           includeMarkdown("about.md")))
+              )
       )
-#       tabItem(tabName = "graphs",
-#               fluidRow(
-#                 column(width = 6,
-#                        box(width = NULL,
-#                            plotlyOutput("capitalGraph"))),
-#                 column(width = 6,
-#                        box(width = NULL,
-#                            plotlyOutput("monroeGraph")
-#                        )
-#                 )
-#               ),
-#               fluidRow(
-#                 column(width = 12,
-#                        box(width = NULL,
-#                            selectInput('hourOfDay', 'Select the hours you are interested', 
-#                                        choices = c(0:23), 
-#                                        selected = NULL, multiple = TRUE,
-#                                        selectize = FALSE, width = NULL, size = NULL)
-#                            ))
-#               ),
-#       tabItem(tabName = "about",
-#               fluidRow(
-#                 column(width = 12,
-#                        box(width = NULL,
-#                            includeMarkdown("about.md")))
-#               )
-#       )
-#       )
-)
+      )
 )
 )
